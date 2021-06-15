87b1cf048d9224d5ec37f23609883ce00f5503fc
Shivansh Goel <shivanshgoel21@gmail.com>
2021-06-15 17:32:59 +0530

    Login.js, Register.js: make login register pages functional

---
+++ src/Pages/Auth/Login.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { useHistory } from "react-router-dom"
+import { useDispatch } from 'react-redux'
+import { SETUSER } from '../../Reducers/actionTypes'
+
+// components
+import Register from './Register'
+import Notification from '../../Components/Notification/Notification'
+
+// styling
+import { Button, Form, Card } from 'react-bootstrap'
+
+// services
+import get from '../../Services/Request/get'
+import post from '../../Services/Request/post'
+
+
+function _Login() {
+    let history = useHistory()
+
+    let moveToRegister = () => {
+        history.push("/register")
+    }
+
+    const dispatch = useDispatch()
+
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+
+    let handleFormSubmit = (event)=>{
+        // login request
+        event.preventDefault()
+
+        if(!email || !password){
+            Notification('Warning','Please type something to proceed!!', "danger")
+            return
+        }
+
+        // set user in store
+        dispatch({
+            type: SETUSER,
+            token: password,
+            username: email,
+            id: 23
+        })
+    }
+
+    return (
+        <Card style={{ margin: "40px", border: "1px solid black" }}>
+            <Card.Header as="h5">Sign In</Card.Header>
+            <Card.Body>
+                <Form style={{ padding: "50px" }} onSubmit={handleFormSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+
+                        <Form.Control type="email" placeholder="Enter email" 
+                            onChange={(event)=>setEmail(event.target.value)}/>
+                    
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" 
+                        onChange={(event)=>setPassword(event.target.value)} />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                    <Button variant="link" onClick={moveToRegister}>New User? Register Here</Button>
+                </Form>
+            </Card.Body>
+        </Card>
+    )
+}
+
+function Login() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/"><_Login/></Route>
+                <Route exact path="/register"><Register/></Route> 
+            </Switch>
+        </Router>
+    )
+}
+
+export default Login
---
+++ src/Pages/Auth/Register.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { Button, Form, Card } from 'react-bootstrap'
+import { useHistory } from "react-router-dom"
+
+// components
+import Notification from '../../Components/Notification/Notification'
+
+// services
+import get from '../../Services/Request/get'
+import post from '../../Services/Request/post'
+
+function Register() {
+    let history = useHistory()
+
+    let moveToLogin = () => {
+        history.push("/")
+    }
+
+    const[email, setEmail]=useState(null)
+    const[password, setPassword]=useState(null)
+    const[cpassword, setcPassword]=useState(null)
+
+    let handleRegister = (event)=>{
+        event.preventDefault();
+        if(!email || !password || !cpassword){
+            Notification('Warning','Please fill all the field to proceed!!', "danger")
+            return
+        }
+
+        // if registered successfully
+        Notification('Voila!','You are registered Successfully!!', "success")
+
+    }
+
+
+    return (
+        <Card style={{margin:"40px", border: "1px solid black"}}>
+            <Card.Header as="h5">Sign Up</Card.Header>
+            <Card.Body>
+                <Form style={{ padding: "50px" }} onSubmit={handleRegister}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email"  onChange={(event)=>setEmail(event.target.value)}/>
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" onChange={(event)=>setPassword(event.target.value)}/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type="password" placeholder="Confirm Password" onChange={(event)=>setcPassword(event.target.value)} />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                    <Button variant="link" onClick={moveToLogin}>Already Registered? Click Here</Button>
+                    <br></br>
+                </Form>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Register

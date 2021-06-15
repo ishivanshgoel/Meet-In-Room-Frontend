041fdb9ac1c440cc35b083269b4baced72554cfd
Shivansh Goel <shivanshgoel21@gmail.com>
2021-06-15 17:33:24 +0530

    Content.js: make content page functional

---
+++ src/Pages/Content/Content.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
+
+// Pages
+import Call from '../Call/Call'
+import Chat from '../Chat/Chat'
+import Teams from '../Teams/Teams'
+
+//styling
+import './CSS/content.css'
+import { Icon } from 'semantic-ui-react'
+
+// routes
+const routes = [
+    { path: 'call', component: Call, name: 'call' },
+    { path: 'chat', component: Chat, name: 'chat' },
+    { path: 'teams', component: Teams, name: 'users' },
+  ]
+  
+
+function Content() {
+    return (
+        <Router>
+        {/* fixed sidebar */}
+        <div class="main">
+          <div class="sidenav">
+            {
+              routes.map((route) => {
+                return <Link to={route.path} class="sidenav-icons"><Icon name={route.name} /></Link>
+              })
+            }
+          </div>
+
+          {/* main div*/}
+          <Switch>
+            {
+              routes.map((route) => {
+                return (<Route exact path={"/" + route.path} component={route.component} />)
+              })
+            }
+          </Switch>
+        </div>
+      </Router>
+    )
+}
+
+export default Content

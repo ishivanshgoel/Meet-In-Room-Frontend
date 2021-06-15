f777ad345bb6d530aa20a103a3c1bd6cff2ad4d5
Shivansh Goel <shivanshgoel21@gmail.com>
2021-06-15 17:31:34 +0530

    App.js: change structure of code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 
-// components
-import Call from './Components/Call/Call'
-import Chat from './Components/Chat/Chat'
-import Teams from './Components/Teams/Teams'
+// Pages
+import Login from './Pages/Auth/Login'
+import Content from './Pages/Content/Content'
 
-//styling
-import { Icon } from 'semantic-ui-react'
+
+//styling components
 import 'semantic-ui-css/semantic.min.css'
+import LoadingScreen from './Components/LoadingScreen/LoadingScreenHook'
+import ReactNotification from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css'
 
 //services
 import online from './Services/Socket/Status/online'
-import reciever from './Services/Socket/Call/reciever';
-
-// routes
-const routes = [
-  { path: 'call', component: Call, name: 'call' },
-  { path: 'chat', component: Chat, name: 'chat' },
-  { path: 'teams', component: Teams, name: 'users' },
-]
+import reciever from './Services/Socket/Call/reciever'
 
 function App() {
+  // loading screen
+  const [loadingScreen, showLoadingScreen, hideLoadingScreen] = LoadingScreen()
 
   useEffect(()=>{
     // pass the user id
-    let id = prompt()
+    // let id = prompt()
+    let id = 'shivansh'
     online(id)
     
      // listeing to incoming calls
     reciever()
+
+    showLoadingScreen()
+
   })  
+
+  // keep track of authenticated user
+  const user = useSelector(state => state.user)
 
   return (
     <>
@@ -40,28 +44,14 @@ function App() {
           <a class="navbar-brand" href="#">Navbar</a>
         </div>
       </nav>
-
-      <Router>
-        {/* fixed sidebar */}
-        <div class="main">
-          <div class="sidenav">
-            {
-              routes.map((route) => {
-                return <Link to={route.path} class="sidenav-icons"><Icon name={route.name} /></Link>
-              })
-            }
-          </div>
-
-          {/* main div*/}
-          <Switch>
-            {
-              routes.map((route) => {
-                return (<Route exact path={"/" + route.path} component={route.component} />)
-              })
-            }
-          </Switch>
-        </div>
-      </Router>
+      <ReactNotification />
+      {
+        ! user ? (
+          <Login/>
+        ) : (
+          <Content />
+        )
+      }
     </>
   );
 }

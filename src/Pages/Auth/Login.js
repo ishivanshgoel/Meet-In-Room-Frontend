f20e7696c8f376f76f5d41ba85bb8f141d96cc6c
Shivansh Goel <shivanshgoel21@gmail.com>
2021-06-16 00:47:14 +0530

    Login.js, Register.js: add loading screen

--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -7,8 +7,10 @@ import { SETUSER } from '../../Reducers/actionTypes'
 // pages
 import Register from './Register'
 import NotFound from '../Static/NotFound'
+
 // compoenents
 import Notification from '../../Components/Notification/Notification'
+import LoadingScreen from '../../Components/LoadingScreen/LoadingScreenHook'
 
 // styling
 import { Button, Form, Card } from 'react-bootstrap'
@@ -27,24 +29,27 @@ function _Login() {
 
     const dispatch = useDispatch()
 
+    const [loadingScreen, showLoadingScreen, hideLoadingScreen] = LoadingScreen()
     const [email, setEmail] = useState(null)
     const [password, setPassword] = useState(null)
 
-    let handleFormSubmit = async (event)=>{
+    let handleFormSubmit = async (event) => {
         // login request
         event.preventDefault()
 
-        if(!email || !password){
-            Notification('Warning','Please type something to proceed!!', "danger")
+        showLoadingScreen()
+
+        if (!email || !password) {
+            Notification('Warning', 'Please type something to proceed!!', "danger")
             return
         }
 
-        const response = await post('login',{
+        const response = await post('login', {
             email,
             password
         })
 
-        if(response.data){
+        if (response.data) {
             let { data } = response
             let { uid, refreshToken } = data
             // set user in redux store
@@ -54,43 +59,48 @@ function _Login() {
                 email: email,
                 user: uid
             })
-        } else{
+        } else {
             // response is error message : string
-            Notification('Error',response, 'danger')
+            Notification('Error', response, 'danger')
         }
+
+        hideLoadingScreen()
 
     }
 
     return (
-        <Card style={{ margin: "40px", border: "1px solid black" }}>
-            <Card.Header as="h5">Sign In</Card.Header>
-            <Card.Body>
-                <Form style={{ padding: "50px" }} onSubmit={handleFormSubmit}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
+        <>
+            {loadingScreen}
+            <Card style={{ margin: "40px", border: "1px solid black" }}>
+                <Card.Header as="h5">Sign In</Card.Header>
+                <Card.Body>
+                    <Form style={{ padding: "50px" }} onSubmit={handleFormSubmit}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
 
-                        <Form.Control type="email" placeholder="Enter email" 
-                            onChange={(event)=>setEmail(event.target.value)}/>
-                    
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    
-                    </Form.Group>
+                            <Form.Control type="email" placeholder="Enter email"
+                                onChange={(event) => setEmail(event.target.value)} />
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" 
-                        onChange={(event)=>setPassword(event.target.value)} />
-                    </Form.Group>
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
 
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                    <Button variant="link" onClick={moveToRegister}>New User? Register Here</Button>
-                </Form>
-            </Card.Body>
-        </Card>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password"
+                                onChange={(event) => setPassword(event.target.value)} />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                        <Button variant="link" onClick={moveToRegister}>New User? Register Here</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </>
     )
 }
 
@@ -98,10 +108,10 @@ function Login() {
     return (
         <Router>
             <Switch>
-                <Route exact path="/"><_Login/></Route>
-                <Route exact path="/register"><Register/></Route> 
+                <Route exact path="/"><_Login /></Route>
+                <Route exact path="/register"><Register /></Route>
                 {/* Not found */}
-                <Route component={NotFound}/>
+                <Route component={NotFound} />
             </Switch>
         </Router>
     )
--- src/Pages/Auth/Register.js
+++ src/Pages/Auth/Register.js
@@ -4,9 +4,9 @@ import { useHistory } from "react-router-dom"
 
 // components
 import Notification from '../../Components/Notification/Notification'
+import LoadingScreen from '../../Components/LoadingScreen/LoadingScreenHook'
 
 // services
-import get from '../../Services/Request/get'
 import post from '../../Services/Request/post'
 
 function Register() {
@@ -16,23 +16,27 @@ function Register() {
         history.push("/")
     }
 
-    const[email, setEmail]=useState(null)
-    const[password, setPassword]=useState(null)
-    const[cpassword, setcPassword]=useState(null)
+    const [loadingScreen, showLoadingScreen, hideLoadingScreen] = LoadingScreen()
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [cpassword, setcPassword] = useState(null)
 
-    let handleRegister = async (event)=>{
-        event.preventDefault();
-        if(!email || !password || !cpassword){
-            Notification('Warning','Please fill all the field to proceed!!', "danger")
+    let handleRegister = async (event) => {
+        event.preventDefault()
+
+        showLoadingScreen()
+
+        if (!email || !password || !cpassword) {
+            Notification('Warning', 'Please fill all the field to proceed!!', "danger")
             return
         }
 
-        const response = await post('register',{
+        const response = await post('register', {
             email,
             password
         })
 
-        if(response.data){
+        if (response.data) {
 
             // de-structure the object to get data out of it 
             let { data } = response.data
@@ -46,46 +50,51 @@ function Register() {
             // })
 
             // if registered successfully
-            Notification('Voila!','You are registered Successfully!!', "success")
+            Notification('Voila!', 'You are registered Successfully!!', "success")
 
         } else {
             // response is error message : string
             Notification('Error', response, 'danger')
         }
 
+        hideLoadingScreen()
+
     }
 
 
     return (
-        <Card style={{margin:"40px", border: "1px solid black"}}>
-            <Card.Header as="h5">Sign Up</Card.Header>
-            <Card.Body>
-                <Form style={{ padding: "50px" }} onSubmit={handleRegister}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email"  onChange={(event)=>setEmail(event.target.value)}/>
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+        <>
+            {loadingScreen}
+            <Card style={{ margin: "40px", border: "1px solid black" }}>
+                <Card.Header as="h5">Sign Up</Card.Header>
+                <Card.Body>
+                    <Form style={{ padding: "50px" }} onSubmit={handleRegister}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" onChange={(event) => setEmail(event.target.value)} />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" onChange={(event)=>setPassword(event.target.value)}/>
-                    </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)} />
+                        </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type="password" placeholder="Confirm Password" onChange={(event)=>setcPassword(event.target.value)} />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                    <Button variant="link" onClick={moveToLogin}>Already Registered? Click Here</Button>
-                    <br></br>
-                </Form>
-            </Card.Body>
-        </Card>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control type="password" placeholder="Confirm Password" onChange={(event) => setcPassword(event.target.value)} />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                        <Button variant="link" onClick={moveToLogin}>Already Registered? Click Here</Button>
+                        <br></br>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </>
     )
 }
 
